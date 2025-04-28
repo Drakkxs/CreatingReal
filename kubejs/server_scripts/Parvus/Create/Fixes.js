a16dfebf38ddbcce42708d9940ac90519a661719
DRAKKKS <70417194+Drakkxs@users.noreply.github.com>
2025-04-28 05:29:55 -0400

    Nothing to report.

---
+++ kubejs/server_scripts/Parvus/Create/Fixes.js
@@ -0,0 +1,23 @@
+// priority: 0
+// @ts-check
+// A script to help with broken recipes or tags.
+
+// Immidately Invoked Function Expression to prevent polluting the global namespace
+(() => {
+
+    /**
+     * Want some debug?
+     */
+    let debug = true
+
+    // Fixing recipes
+    ServerEvents.recipes(event => {
+        
+    })
+
+    ServerEvents.tags('fluid', event => {
+
+        event.add("c:molten_enderium", "createmechanisms:enderiam")
+    })
+
+})()
