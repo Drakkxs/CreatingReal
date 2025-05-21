77a5fe1314ae32cf41d358f011ed501744976bb3
DRAKKKS <70417194+Drakkxs@users.noreply.github.com>
2025-05-21 05:39:53 -0400

    Refactor: Simplify cold mechanism recipe mapping by using mapTags function

--- kubejs/server_scripts/Parvus/CreateMechanisms/ColdMechanismRecipeShaped.js
+++ kubejs/server_scripts/Parvus/CreateMechanisms/ColdMechanismRecipeShaped.js
@@ -2,27 +2,32 @@
 // requires: createmechanisms
 // New mechanism for createmechanisms.
 ServerEvents.recipes(event => {
-   event.shaped(
-       Item.of('kubejs:cold_mechanism', 1), // arg 1: output
-       [
-           ' d ',
-           'aba', // arg 2: the shape (array of strings)
-           ' c '
-       ],
-       {
-           a: ['minecraft:powder_snow_bucket']
-           // Use tags instead of static items
-           .map(a => getUnifiedTag(a))[0],
-
-           b: ['createmechanisms:rubber_mechanism']
-           .map(a => getUnifiedTag(a))[0],  //arg 3: the mapping object
-
-           c: ['minecraft:obsidian']
-           .map(a => getUnifiedTag(a))[0],
-
-           d: ['minecraft:blue_ice']
-           .map(a => getUnifiedTag(a))[0]
-           
-       }
-   )
+    /**
+         * Maps the values of the given object to their unified tags using getUnifiedTag.
+         * @param {Object<string, any>} mapping - An object where each key maps to a value to be unified.
+         * @returns {Object<string, any>} A new object with the same keys, where each value is the result of getUnifiedTag.
+         */
+    function mapTags(mapping) {
+        let result = {};
+        for (let key in mapping) {
+            let value = mapping[key];
+            // Only call getUnifiedTag if value is a string (item ID), otherwise pass through (e.g., Ingredient)
+            result[key] = (Item.isItem(value)) ? getUnifiedTag(value) : value;
+        }
+        return result
+    }
+    event.shaped(
+        Item.of('kubejs:cold_mechanism', 1),
+        [
+            ' d ',
+            'aba',
+            ' c '
+        ],
+        mapTags({
+            a: 'minecraft:powder_snow_bucket',
+            b: 'createmechanisms:rubber_mechanism',
+            c: 'minecraft:obsidian',
+            d: 'minecraft:blue_ice'
+        })
+    )
 })
