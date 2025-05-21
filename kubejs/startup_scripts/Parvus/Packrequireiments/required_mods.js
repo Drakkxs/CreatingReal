f83b30a9553ae534cbc49a57ccd5feb131a7b015
DRAKKKS <70417194+Drakkxs@users.noreply.github.com>
2025-05-21 05:40:33 -0400

    Add: Create required_mods.js to define mod dependencies for the pack

---
+++ kubejs/startup_scripts/Parvus/Packrequireiments/required_mods.js
@@ -0,0 +1,22 @@
+// This file is stops the pack from loading if the requirements are not met
+// requires: modular_machinery_reborn 
+// requires: createcompounds
+// requires: almostunified
+// requires: create
+
+/**
+ * Modular Machinery Reborn is the core mod for the machines.
+ * It is required for the pack to function and the multi-blocks to exist.
+ * It creates a purpose for Create Mechanisms, and Create in general to be included in the pack.
+ * 
+ * Create Compounds is required for the Nether Star recipe.
+ * It is not required for the pack to function, 
+ * but is recommended for nether star automation, dye duplicating, 
+ * and other features only available through the provided higher tier materials.
+ * 
+ * Almost Unified is required for the pack to function.
+ * It is used to unify the tags and items in the pack, and it's helper functions are used in recipes.
+ * 
+ * Other mods like Mekanism, Industrial Forgoing, Immersive Engineering, Applied Energistics 2, or Refined Storage
+ * are not required for the pack to function, but are recommended for automation and storage.
+ */
