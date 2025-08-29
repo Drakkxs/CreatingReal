bc1107221112a9d1b3ef14df54413feffb758dde
DRAKKKS <70417194+Drakkxs@users.noreply.github.com>
2025-08-29 04:47:28 -0400

    Add: Implement Beta Cloche crafting recipe and improve Basic Cloche recipe

---
+++ kubejs/server_scripts/Parvus/OpoliusUtitlites/BetaCloche.js
@@ -0,0 +1,43 @@
+// priority: 0
+// requires: minecraft
+// @ts-check
+// Recipe for the Beta Cloche and a better recipe for the Basic Cloche
+
+// Immidately Invoked Function Expression to prevent polluting the global namespace
+(() => {
+    let basicCloche = "cloche:cloche"
+    let betaClocheId = "opolisutilities:cloche" // Beta Cloche
+
+    function getVariantItem(item) {
+        return AlmostUnified.getVariantItemTarget(item).idLocation.toString();
+    }
+    
+    ServerEvents.recipes(event => {
+
+        // If there is already a recipe that outputs the Beta Cloche skip adding a recipe.
+        if (event.findRecipes({ output: betaClocheId }).size()) return;
+
+        // Remove existing recipe for the Basic Cloche
+        event.remove({ output: basicCloche, type: "minecraft:crafting_shaped" });
+
+        event.shaped(betaClocheId, [
+            "MMM",
+            "ICI",
+            "MMM"
+        ], {
+            M: getVariantItem("minecraft:copper_ingot"),
+            C: getVariantItem("minecraft:dirt"),
+            I: getVariantItem("minecraft:water_bucket")
+        })
+
+        event.shaped(betaClocheId, [
+            "MMM",
+            "ICI",
+            "MMM"
+        ], {
+            M: getVariantItem("minecraft:iron_ingot"),
+            C: basicCloche,
+            I: getVariantItem("minecraft:water_bucket")
+        })
+    })
+})()
